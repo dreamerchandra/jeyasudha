751ef620be04ca1ba1ded025ba8e252828f7a8f8
dreamerchandra <17010408+dreamerchandra@users.noreply.github.com>
2020-10-11 15:59:36 +0530

    arduino -> jeyasudha firebase

--- client/src/js/firebase-init.js
+++ client/src/js/firebase-init.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 
 export default function initializeFirebase() {
   const firebaseConfig = {
-    apiKey: 'AIzaSyA0P_eopNWIL23qYYnu4FOxkID6SK2zBLA',
-    authDomain: 'arudino1-3381d.firebaseapp.com',
-    databaseURL: 'https://arudino1-3381d.firebaseio.com',
-    projectId: 'arudino1-3381d',
-    storageBucket: 'arudino1-3381d.appspot.com',
-    messagingSenderId: '1004597171360',
-    appId: '1:1004597171360:web:bf073b9fd7b802a898a5c5',
+    apiKey: 'AIzaSyAfbjKQKQ3eueeHS9XVyIpJnoG9DR6isJs',
+    authDomain: 'jeyasudha-716bd.firebaseapp.com',
+    databaseURL: 'https://jeyasudha-716bd.firebaseio.com',
+    projectId: 'jeyasudha-716bd',
+    storageBucket: 'jeyasudha-716bd.appspot.com',
+    messagingSenderId: '913579087424',
+    appId: '1:913579087424:web:f9ff1e7c725cd02d30f1a5',
   }
   console.log('initalizing app')
   window.firebase = initializeApp(firebaseConfig)
