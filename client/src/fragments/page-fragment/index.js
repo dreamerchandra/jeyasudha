e90fb4fd3518d37a94ba8d13023b4e7db7f77491
dreamerchandra <17010408+dreamerchandra@users.noreply.github.com>
2020-10-30 23:22:57 +0530

    css refactor

--- client/src/fragments/page-fragment/index.js
+++ client/src/fragments/page-fragment/index.js
@@ -4,13 +4,16 @@ import 'react-toastify/dist/ReactToastify.css'
 import NavBar from '../nav'
 import Header from '../../components/header'
 import AuthenticatedView from '../../components/authenticated-view'
+import './index.css'
 
 const PageFragment = ({ children }) => {
   return (
     <AuthenticatedView>
-      <Header />
       <NavBar />
-      <>{children}</>
+      <div className="main-body-holder">
+        <Header />
+        <>{children}</>
+      </div>
       <ToastContainer
         position="top-right"
         autoClose={5000}
