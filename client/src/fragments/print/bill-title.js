ab98f99f7371ebd0b4b5a2e198d9206967eba7ba
dreamerchandra <17010408+dreamerchandra@users.noreply.github.com>
2021-02-27 01:06:20 +0530

    added address to the bill

--- client/src/fragments/print/bill-title.js
+++ client/src/fragments/print/bill-title.js
@@ -4,8 +4,15 @@ const BillTitle = () => (
   <>
     <div className="title">
       <h2>Jeyasudha Blue Metals</h2>
+      <div className="address">
+        <p>Near by Rajshree Sugar Factory,</p>
+        <p>Periyakulam Road, Varatharaj Nagar,</p>
+        <p>Vaigaidam, Andipatty 625512</p>
+      </div>
     </div>
-    <p className="s-center">Cash/Credit</p>
+    <p className="s-center" style={{ fontSize: '24px' }}>
+      Cash/Credit
+    </p>
   </>
 )
 
